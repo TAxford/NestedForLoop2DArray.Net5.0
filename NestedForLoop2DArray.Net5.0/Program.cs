8e9f87c57ea646ec53ffaa7d247b4cf21c51d7ae
Timothy Axford <t.axford2@yahoo.com>
2023-03-20 13:06:28 -0400

    Created a foreach loop to print the items in the matrix

--- NestedForLoop2DArray.Net5.0/Program.cs
+++ NestedForLoop2DArray.Net5.0/Program.cs
@@ -8,12 +8,15 @@ namespace NestedForLoop2DArray.Net5._0
         {
             {1,2,3},
             {4,5,6},
-            {7,8,9},
+            {7,8,9}
         };
 
         static void Main(string[] args)
         {
-            
+            foreach (int item in matrix)
+            {
+                Console.Write(item + " ");
+            }
         }
     }
 }
