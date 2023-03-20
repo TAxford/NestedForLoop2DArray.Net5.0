31cefd1463ee63bdbce884ae556029b84aa927f4
Timothy Axford <t.axford2@yahoo.com>
2023-03-20 13:04:03 -0400

    Added static int matrix

--- NestedForLoop2DArray.Net5.0/Program.cs
+++ NestedForLoop2DArray.Net5.0/Program.cs
@@ -4,9 +4,16 @@ namespace NestedForLoop2DArray.Net5._0
 {
     internal class Program
     {
+        static int[,] matrix =
+        {
+            {1,2,3},
+            {4,5,6},
+            {7,8,9},
+        };
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
         }
     }
 }
